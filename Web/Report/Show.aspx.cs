dfac9ad37d3bbbc49a18ee78acd6f11937ac4af3
Bryantzhangxin <121909522@qq.com>
2014-08-04 10:22:02 +0800

    111

--- Web/Report/Show.aspx.cs
+++ Web/Report/Show.aspx.cs
@@ -12,34 +12,34 @@ using System.Text;
 namespace Maticsoft.Web.Report
 {
     public partial class Show : Page
-    {        
-        		public string strid=""; 
-		protected void Page_Load(object sender, EventArgs e)
-		{
-			if (!Page.IsPostBack)
-			{
-				if (Request.Params["id"] != null && Request.Params["id"].Trim() != "")
-				{
-					strid = Request.Params["id"];
-					int ID=(Convert.ToInt32(strid));
-					ShowInfo(ID);
-				}
-			}
-		}
-		
-	private void ShowInfo(int ID)
-	{
-		Maticsoft.BLL.Report bll=new Maticsoft.BLL.Report();
-		Maticsoft.Model.Report model=bll.GetModel(ID);
-		this.lblID.Text=model.ID.ToString();
-		this.lblname.Text=model.name;
-		this.lblprojectName.Text=model.projectName;
-		this.lbltime.Text=model.time.ToString();
-		this.lbltodayWork.Text=model.todayWork;
-		this.lblproblem.Text=model.problem;
-		this.lbltomorrowWork.Text=model.tomorrowWork;
+    {
+        public string strid = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                if (Request.Params["id"] != null && Request.Params["id"].Trim() != "")
+                {
+                    strid = Request.Params["id"];
+                    int ID = (Convert.ToInt32(strid));
+                    ShowInfo(ID);
+                }
+            }
+        }
 
-	}
+        private void ShowInfo(int ID)
+        {
+            Maticsoft.BLL.Report bll = new Maticsoft.BLL.Report();
+            Maticsoft.Model.Report model = bll.GetModel(ID);
+            this.lblID.Text = model.ID.ToString();
+            this.lblname.Text = model.name;
+            this.lblprojectName.Text = model.projectName;
+            this.lbltime.Text = model.time.ToString();
+            this.lbltodayWork.Text = model.todayWork;
+            this.lblproblem.Text = model.problem;
+            this.lbltomorrowWork.Text = model.tomorrowWork;
+
+        }
 
 
     }
